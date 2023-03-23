f8c2c9511bbe6a6400cb44be7d2c4f875bf6e4ff
Omar Aviles <oaviles@live.com>
2023-03-23 13:49:09 -0600

    Update connect_to_cluster.sh

--- cli/connect_to_cluster.sh
+++ cli/connect_to_cluster.sh
@@ -1,6 +1,6 @@
 REGION_NAME=eastus2
-RESOURCE_GROUP=POC-ARO-002
-CLUSTER_NAME=oa-aro-poc-002
+RESOURCE_GROUP=aro-work-898170
+CLUSTER_NAME=oa-aro-poc-003
 
 
 az login --service-principal -u $1 -p $2 --tenant $3
