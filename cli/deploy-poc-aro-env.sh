6af2c7868bc50b003483944dfa0a077367818524
Omar Aviles <oaviles@live.com>
2023-03-23 10:27:06 -0600

    Update deploy-poc-aro-env.sh

--- cli/deploy-poc-aro-env.sh
+++ cli/deploy-poc-aro-env.sh
@@ -1,5 +1,5 @@
 LOCATION=eastus2                # the location of your cluster
-RESOURCEGROUP=aro-work-898140           # the name of the resource group where you want to create your cluster
+RESOURCEGROUP=POC-ARO-003           # the name of the resource group where you want to create your cluster
 CLUSTER=oa-aro-poc-003                 # the name of your cluster
 
 az login --service-principal -u $1 -p $2 --tenant $3
