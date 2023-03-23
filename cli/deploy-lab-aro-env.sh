d4eaa2d26c2b8cc8d386a1d7234ec5d24780f7ca
Omar Aviles <oaviles@live.com>
2023-03-23 10:29:24 -0600

    Create deploy-lab-aro-env.sh

---
+++ cli/deploy-lab-aro-env.sh
@@ -0,0 +1,7 @@
+LOCATION=eastus2                # the location of your cluster
+RESOURCEGROUP=aro-work-898140           # the name of the resource group where you want to create your cluster
+CLUSTER=oa-aro-poc-003                 # the name of your cluster
+
+az login --service-principal -u $1 -p $2 --tenant $3
+
+az aro create --resource-group $RESOURCEGROUP --name $CLUSTER --vnet aro-vnet --master-subnet master-subnet --worker-subnet worker-subnet --worker-count 3 --client-id $1 --client-secret $2 -o table
